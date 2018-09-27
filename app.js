1f69edf82cf0edb4f354b67181af1aba3a9ee026
unknown <TyranoPT>
2018-09-27 18:06:11 +0100

    Const's createad and other dependecies installed

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+const path = require('path');
 const express = require('express');
+const database = require('./config/database');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
+
+const app = express();
+
+const connection = mysql.createConnection({
+  host     : database.host,
+  user     : database.user,
+  password :database.password,
+  database : database.database,
+  port : database.port
+});
+
+connection.connect((err)=>{
+  if(err){
+    console.log(err);
+  }
+});
---
+++ config/database.js
@@ -0,0 +1,8 @@
+module.exports= {
+    host: 'lhcp1068.webapps.net',
+    user:'np1zf8rb_LusiAD',
+    pass:'gg}SIB%xnOC,',
+    database:'np1zf8rb_SD_Manager_Lusi',
+    port:3306
+
+}
