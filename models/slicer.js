47983774d959669e693e4b97437eaa7b308c2a80
Tiago Dias <tiago024@gmail.com>
2018-10-07 14:41:20 +0100

    optimezed connection

--- models/slicer.js
+++ models/slicer.js
@@ -1,11 +1,12 @@
 const db = require('../config/dbconnection');
 
 module.exports.getSlider= (id,callback)=>{
-  db.query('Select * from sliderimages where FKSlider = ? ',id,(error,results,fields)=>{
+  db.query('Select * from sliderimages Where FKSlider = ? ',id,(error,results,fields)=>{
+    if(error) throw error;
     var paths=[];
     results.forEach(function(r){
         paths.push(r.path);
-        });
+      });
     return callback(paths);
   });
 }
