bb6107a4be13813a56d34c4a8b6345ee802571bc
unknown <TyranoPT>
2018-10-16 18:52:02 +0100

    Blur effect on book. Improved footer.

--- angular-src/src/app/components/footer/footer.component.ts
+++ angular-src/src/app/components/footer/footer.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-footer',
+  selector: 'footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
@@ -10,9 +10,14 @@ export class FooterComponent implements OnInit {
 
   constructor(private router:Router) { }
   isHome:any;
+  name:String;
+  email:String;
+  message:String;
   ngOnInit() {
    this.isHome = (this.router.url !='/'? false:true);
   //  this.isHome = this.router.url;
   }
+ onContactSubmit(){
 
+ }
 }
